f771293700dbd0b3f5be062bab37089a792fb616
Shavon Harris <Shavonharris9114@gmail.com>
2024-09-23 11:48:37 -0500

    set up get and post request, started ui

--- magical_mirroir/app.py
+++ magical_mirroir/app.py
@@ -1,23 +1,34 @@
 #define Routes for REST Endpoints
 
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
 
-messages = []
+messages = [
+     {"message": "You are doing amazing! Keep going."},
+    {"message": "Don't forget, you're loved and appreciated."}
+]
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/api/messages', methods=['GET'])
+@cross_origin()
 def get_messages():
-    return jsonify(messages), 200
+    return jsonify(messages),200
  
-@app.route('/api/messages', methods=['POST'])
+@app.route('/api/new_message', methods=['POST'])
 def add_message():
-    new_message = request.get_json('message')
+    # Fetch the new message from the request
+    data = request.get_json()
+    new_message = data.get('message')
+
     if new_message:
-        messages.append(new_message)
+        # Add the new message to the list of messages
+        messages.append({"message": new_message})
         return jsonify({"message": "Message added successfully!"}), 201
     else:
         return jsonify({"error": "No message provided"}), 400
